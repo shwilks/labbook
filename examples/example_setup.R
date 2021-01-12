c59c2732bec89ac716353eaeee9834f0e004ec7b
Sam Wilks <sw463@cam.ac.uk>
2021-01-12 19:34:13 +0100

    replace examples

---
+++ examples/example_setup.R
@@ -0,0 +1,8 @@
+
+# Intiate a new labbook on the desktop with a first project
+labbook.init(
+    labbook.dir   = "~/Desktop",
+    labbook.title = "Sam's labbook",
+    project.title = "B cell analysis"
+)
+
