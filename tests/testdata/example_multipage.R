b71aceb7a53ca70c825aa8084b03b9ce9487b9d8
Sam Wilks <sw463@cam.ac.uk>
2024-02-21 15:20:10 +1100

    Add a multipage example

---
+++ tests/testdata/example_multipage.R
@@ -0,0 +1,28 @@
+
+##' Subtitle one
+###' An example multipage page
+#'
+#' ## Aim
+#' This is the aim of the page and a plot:
+
+# General setup
+library(labbook)
+rm(list = ls())
+
+message("Rendering general setup")
+
+#' ### Multiple pages
+
+out.pageset({
+    for (x in 1:3) {
+
+        out.page(
+            paste("page", x), {
+
+            plot(cars, col = rainbow(3)[x])
+            message(sprintf("Rendering page %s", x))
+
+        })
+
+    }
+})
